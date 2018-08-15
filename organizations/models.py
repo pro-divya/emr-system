579c01757990eb27373d1246b2ec7c316e0b680a
pro-divya <ksso8901@gmail.com>
2018-08-15 16:52:54 +0700

    28-as-a-medidata-admin-i-want-to-se: Auto stash before merge of "MEDI-28-as-a-medidata-admin-i-want-to-se" and "MEDI-10-as-medidata-admin-i-would-like-t"
    edit organisation model

--- organizations/models.py
+++ organizations/models.py
@@ -2,7 +2,29 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-class OrganizationClient(models.Model):
+class OrganizationBase(models.Model):
+    trading_name = models.CharField(max_length=255)
+    legal_name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact_name = models.CharField(max_length=255)
+    contact_telephone = PhoneNumberField()
+    contact_email = models.EmailField()
+    generic_telephone = PhoneNumberField()
+    generic_email = models.EmailField()
+    fax_number = PhoneNumberField()
+    companies_house_number = models.CharField(max_length=255)
+    vat_number = models.CharField(max_length=255)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    deleted_timestamp = models.DateTimeField(editable=False, null=True)
+
+    class Meta:
+        verbose_name = 'Organization'
+
+    def __str__(self):
+        return self.trading_name
+
+
+class OrganizationClient(OrganizationBase):
     INSURANCE_UNDERWRITER = 1
     INSURANCE_CLAIM = 2
     MEDIDATA = 3
@@ -15,24 +37,11 @@ class OrganizationClient(models.Model):
         (MEDICOLEGAL, 'Medicolegal')
     )
 
-    trading_name = models.CharField(max_length=255)
-    legal_name = models.CharField(max_length=255)
-    address = models.TextField()
     type = models.IntegerField(choices=ROLE_CHOICES)
-    contact_name = models.CharField(max_length=255, blank=True)
-    contact_telephone = PhoneNumberField(blank=True)
-    contact_email = models.EmailField(blank=True)
-    generic_telephone = PhoneNumberField(blank=True)
-    generic_email = models.EmailField(blank=True)
-    fax_number = PhoneNumberField(blank=True)
-    companies_house_number = models.CharField(max_length=255, blank=True)
-    vat_number = models.CharField(max_length=255, blank=True)
     fca_number = models.CharField(max_length=255, blank=True)
     division = models.TextField(blank=True)
     can_create_amra = models.BooleanField(default=False)
     can_create_sars = models.BooleanField(default=False)
-    created_timestamp = models.DateTimeField(auto_now_add=True)
-    deleted_timestamp = models.DateTimeField(editable=False, null=True)
 
     class Meta:
         verbose_name = 'Organization Client'
@@ -41,7 +50,7 @@ class OrganizationClient(models.Model):
         return self.trading_name
 
 
-class OrganizationGeneralPractice(models.Model):
+class OrganizationGeneralPractice(OrganizationBase):
     GP_OP_SYS_CHOICES = (
         ('EW', 'EMIS-Web'),
         ('EP', 'EMIS-PCS'),
@@ -58,27 +67,14 @@ class OrganizationGeneralPractice(models.Model):
         ('AD', 'Advance')
     )
 
-    trading_name = models.CharField(max_length=255)
-    legal_name = models.CharField(max_length=255)
     operating_system = models.CharField(max_length=2, choices=GP_OP_SYS_CHOICES)
     operating_system_socket_endpoint = models.CharField(max_length=255)
     operating_system_auth_token = models.CharField(max_length=255)
-    address = models.TextField()
-    contact_name = models.CharField(max_length=255)
-    contact_telephone = PhoneNumberField()
-    contact_email = models.EmailField()
-    generic_telephone = PhoneNumberField()
-    generic_email = models.EmailField()
-    fax_number = PhoneNumberField()
-    companies_house_number = models.CharField(max_length=255)
-    vat_number = models.CharField(max_length=255)
     practice_code = models.CharField(max_length=255)
     payment_timing = models.CharField(max_length=2, choices=PAYMENT_TIMING_CHOICES)
     payment_bank_holder_name = models.CharField(max_length=255)
     payment_bank_sort_code = models.CharField(max_length=255)
     payment_bank_account_number = models.CharField(max_length=255)
-    created_timestamp = models.DateTimeField(auto_now_add=True)
-    deleted_timestamp = models.DateTimeField(editable=False, null=True)
 
     class Meta:
         verbose_name = 'Organization GeneralPractice'
