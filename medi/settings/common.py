721431fec120e6172ad7de41b0252f03ccadd9e1
Thitthiphong Naranongwang <thitthiphong.n@gmail.com>
2018-09-13 17:04:32 +0700

    DATABASE sqlite3

--- medi/settings/common.py
+++ medi/settings/common.py
@@ -96,23 +96,12 @@ WSGI_APPLICATION = 'medi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }postgres://localhost/medidata_development
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'medidata',
-        # 'USER': 'medi',
-        # 'PASSWORD': 'medi',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
