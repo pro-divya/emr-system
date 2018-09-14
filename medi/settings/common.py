97e45cdfb2e1110f455e28130cca270c4dd6a5ce
pro-divya <ksso8901@gmail.com>
2018-09-14 15:04:06 +0700

    change dummy mail

--- medi/settings/common.py
+++ medi/settings/common.py
@@ -162,7 +162,7 @@ EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'akekatharn@mohara.co'
+EMAIL_HOST_USER = 'mohara.qr@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('MAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
