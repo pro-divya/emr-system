a2de070852269f5b2d41a2dfd78a2dc5266dadc3
pro-divya <ksso8901@gmail.com>
2018-09-14 15:46:17 +0700

    fixed bug date format

--- medi/settings/common.py
+++ medi/settings/common.py
@@ -178,3 +178,8 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger'
 }
+
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y',     # 25/10/2006
+    '%d/%m/%y',     # 25/10/06
+]
