328ffac8ae9cfe4787c3eeeb2201ed72b5452bef
Pichate Buakaew <pichate@mohara.co>
2018-09-28 08:12:50 +0000

    Merged in test (pull request #54)
    Update files
    
    * Update files
    
    * Update file
    
    * Test
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Update
    
    * Add cache
    
    * Add path
    
    * Update deploy
    
    * Update file
    
    * Update
    
    * Update fiel
    
    * Test deploy
    
    * Update key
    
    * Change deploy
    
    * Update
    
    * Update path file
    
    * Test CI
    
    * Add key
    
    * Test deploy
    
    * Update files
    
    * Add workflow
    
    * Add work directory
    
    * Change work directory
    
    * Change run command
    
    * Update
    
    * Update key
    
    * Delete key
    
    * Update CI
    
    * Updated
    
    * Delete files
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- .servers/.deploy-uat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USERNAME=mohara
-HOSTS="web02-cl02-mde.flexiion-customer.net"
-SCRIPT="cd medi;git pull origin development;docker exec -i medi_djangoapp_1 python manage.py migrate"
-for HOSTNAME in ${HOSTS} ; do
-    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-done
