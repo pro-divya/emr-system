a4943bbd19026c201a06c66169439075aba65e7d
pro-divya <ksso8901@gmail.com>
2018-10-01 11:58:43 +0000

    Merged in MEDI-151-input-date-at-organisation-fee- (pull request #58)
    fixed payment fee input in admin
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- payment/admin.py
+++ payment/admin.py
@@ -5,6 +5,14 @@ from .forms import OrganisationFeeForm
 
 class OrganisationFeeAdmin(admin.ModelAdmin):
     form = OrganisationFeeForm
+    fields = (
+        'gp_practice',
+        'max_day_lvl_1', 'max_day_lvl_2', 'max_day_lvl_3',
+        'amount_rate_lvl_1', 'amount_rate_lvl_2', 'amount_rate_lvl_3'
+    )
+
+    class Media:
+        js = ('js/custom_admin/payment_fee_admin.js', )
 
 
 admin.site.register(OrganisationFee, OrganisationFeeAdmin)
--- payment/forms.py
+++ payment/forms.py
@@ -3,6 +3,7 @@ from .models import OrganisationFee
 
 
 class OrganisationFeeForm(forms.ModelForm):
+    max_day_lvl_3 = forms.IntegerField(required=False,label='Min day lvl 3', widget=forms.NumberInput(attrs={'class': 'vIntegerField'}))
 
     class Meta:
         model = OrganisationFee
@@ -10,16 +11,14 @@ class OrganisationFeeForm(forms.ModelForm):
 
     def clean(self):
         try:
-            max_day_lvl_1 = self.cleaned_data['max_day_lvl_1']
-            max_day_lvl_2 = self.cleaned_data['max_day_lvl_2']
-            max_day_lvl_3 = self.cleaned_data['max_day_lvl_3']
-            if max_day_lvl_1 >= max_day_lvl_3:
+            self.cleaned_data['max_day_lvl_3'] = self.cleaned_data['max_day_lvl_2'] + 1
+            if self.cleaned_data['max_day_lvl_1'] >= self.cleaned_data['max_day_lvl_3']:
                 raise forms.ValidationError("Day are incorrect: Max day lvl 3 must more than Max day lvl 1")
 
-            if max_day_lvl_1 >= max_day_lvl_2:
+            if self.cleaned_data['max_day_lvl_1'] >= self.cleaned_data['max_day_lvl_2']:
                 raise forms.ValidationError("Day are incorrect: Max day lvl 2 must more than Max day lvl 1")
 
-            if max_day_lvl_2 >= max_day_lvl_3:
+            if self.cleaned_data['max_day_lvl_2'] >= self.cleaned_data['max_day_lvl_3']:
                 raise forms.ValidationError("Day are incorrect: Max day lvl 3 must more than Max day lvl 2")
 
             return self.cleaned_data
