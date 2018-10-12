bf087fc060b06905a988df2299b8fe1d7861fdfd
Bench <pichate@mohara.co>
2018-10-12 10:14:10 +0700

    Update

--- medi/settings/local_settings.py
+++ medi/settings/local_settings.py
@@ -7,9 +7,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'medidata',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'NAME': 'medi_test',
+        'USER': 'waltzhargen',
+        'PASSWORD': '',
+        'HOST': '',
     }
 }
+
+EMIS_API_HOST = 'http://medi.mohub.co:3001/emis'
