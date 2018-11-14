a9815d25813e43ad90839986800fe1de13fcce6c
markmanu <markmanu@yandex.com>
2018-11-14 19:44:36 +0400

    fix form field issue

--- accounts/forms.py
+++ accounts/forms.py
@@ -52,13 +52,13 @@ class PatientForm(forms.ModelForm):
 
 
 class GPForm(forms.Form):
-    gp_title = forms.ChoiceField(choices=TITLE_CHOICE, required=False)
+    title = forms.ChoiceField(choices=TITLE_CHOICE, required=False)
     initial = forms.CharField(max_length=255, required=False, label='Initials', widget=forms.TextInput(attrs={'placeholder': ''}))
-    gp_last_name = forms.CharField(max_length=255, required=False, label='Last Name', widget=forms.TextInput(attrs={'id': 'gp_last_name', 'name': 'gp_last_name', 'placeholder': '',}))
+    last_name = forms.CharField(max_length=255, required=False, label='Last Name', widget=forms.TextInput(attrs={'id': 'gp_last_name', 'name': 'gp_last_name', 'placeholder': '',}))
 
     class Meta:
         model = GeneralPracticeUser
-        fields = ('title', 'initial', 'gp_last_name')
+        fields = ('title', 'initial', 'last_name')
 
         labels = {
             'gp_title': 'Title'
