0671e5dd88ec61cc000f968533067b738216e872
Pichate Buakaew <pichate@mohara.co>
2018-12-06 05:16:32 +0000

    Merged in MEDI-380-pipeline-view-fails-with-attrib (pull request #382)
    Move call api
    
    * Move call api
    
    * Update text
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- instructions/urls.py
+++ instructions/urls.py
@@ -8,6 +8,7 @@ urlpatterns = (
     path('view-reject/<int:instruction_id>/', views.view_reject, name='view_reject'),
     path('upload-consent/<int:instruction_id>/', views.upload_consent, name='upload_consent'),
     path('review-instruction/<int:instruction_id>', views.review_instruction, name='review_instruction'),
+    path('api-get-address/<str:address>', views.api_get_address, name='api_get_address'),
     path('consent-contact/<int:instruction_id>/select-patient/<int:patient_emis_number>', views.consent_contact, name='consent_contact'),
     path('print-mdx-consent/<int:instruction_id>/patient/<int:patient_emis_number>', views.print_mdx_consent, name='print_mdx_consent')
 )
