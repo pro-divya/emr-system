584a92a95c9876d4d2c56f875f02180409a0dfd2
Mr.Book <jirayu.oopi@gmail.com>
2018-12-11 17:58:54 +0700

    Edit File because wrong implement :(

--- instructions/views.py
+++ instructions/views.py
@@ -314,7 +314,7 @@ def new_instruction(request):
             )
             messages.success(request, 'Form submission successful')
             if instruction.type == SARS_TYPE and request.user.type == GENERAL_PRACTICE_USER:
-                return redirect('medicalreport:set_patient_emis_number', instruction_id=instruction.id)
+                return redirect('instructions:review_instruction', instruction_id=instruction.id)
             else:
                 return redirect('instructions:view_pipeline')
         else:
