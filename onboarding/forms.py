102935730022b104a8388adf695be621e4eba181
Jirayu <jirayu@mohara.co>
2019-01-03 03:57:32 +0000

    Merged in MEDI-470-the-postcode-search-doesnt-alwa (pull request #503)
    MEDI-470 the postcode search doesnt alwa
    
    * Deleted unuse form in search bar. ( Complete )
    
    * Edit label contry and set input contry to optional !
    
    * Fix the input for show patien_email.
    
    * Edit word about country to county.
    
    * Push another card in this branch.
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- onboarding/forms.py
+++ onboarding/forms.py
@@ -15,7 +15,7 @@ class SurgeryForm(forms.Form):
     address_line2 = forms.CharField(max_length=255, label='', widget=forms.TextInput(), required=False)
     address_line3 = forms.CharField(max_length=255, label='', widget=forms.TextInput(), required=False)
     city = forms.CharField(max_length=20, label='', widget=forms.TextInput())
-    country = forms.CharField(max_length=20, label='', widget=forms.TextInput())
+    county = forms.CharField(max_length=20, label='', widget=forms.TextInput())
     contact_num = forms.CharField(max_length=20, label='', widget=forms.TextInput())
     emis_org_code = forms.CharField(max_length=20, label='', widget=forms.TextInput(), required=False)
     operating_system = forms.ChoiceField(choices=OrganisationGeneralPractice.GP_OP_SYS_CHOICES, label='')
