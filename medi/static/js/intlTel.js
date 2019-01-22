2a1945d357043e1291b924154a228bfc814ec2cd
Pichate Buakaew <pichate@mohara.co>
2019-01-22 06:33:34 +0000

    Merged in MEDI-579-selecting-the-country-code-more (pull request #590)
    Update JS for phone number
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- medi/static/js/intlTel.js
+++ medi/static/js/intlTel.js
@@ -1,37 +1,52 @@
-function setDefaultCountry(phoneID, codeID){
-  $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
-    var countryCode = (resp && resp.country) ? resp.country : "";
-    countryCode = countryCode.toLocaleLowerCase();
-    var phoneCode = $(".country-list li[data-country-code=" + countryCode + "]").attr("data-dial-code");
-    var telephoneCode = $("#id_patient_telephone_code").val();
-    if(telephoneCode){
-      countryCode = $(".country-list li[data-dial-code=" + telephoneCode + "]").attr("data-country-code");
-      phoneCode = telephoneCode;
-    }
-    $("#" + codeID).parent().find(".intl-tel-input").addClass("iti-sdc-3");
-    $("#" + codeID).parent().find(".selected-flag").attr("title", "+" + phoneCode);
-    $("#" + codeID).parent().find(".selected-flag .iti-flag").addClass(countryCode);
-    $("#" + codeID).parent().find(".selected-flag .selected-dial-code").html("+" + phoneCode);
-    $("#" + codeID).val(phoneCode);
-  });
+function setDefaultCountry(phoneID, codeID, countryCode){
+  var phoneCode = $(".country-list li[data-country-code=" + countryCode + "]").attr("data-dial-code");
+  var telephoneCode = $("#id_patient_telephone_code").val();
+  if(telephoneCode){
+    countryCode = $(".country-list li[data-dial-code=" + telephoneCode + "]").attr("data-country-code");
+    phoneCode = telephoneCode;
+  }
+  $("#" + codeID).parent().find(".intl-tel-input").addClass("iti-sdc-3");
+  $("#" + codeID).parent().find(".selected-flag").attr("title", "+" + phoneCode);
+  $("#" + codeID).parent().find(".selected-flag .iti-flag").addClass(countryCode);
+  $("#" + codeID).parent().find(".selected-flag .selected-dial-code").html("+" + phoneCode);
+  $("li[data-country-code='" + countryCode + "']").addClass("active");
+  $("li[data-country-code='" + countryCode + "']").attr("aria-selected", true);
+  $("#country-listbox").attr("aria-activedescendant", "iti-item-" + countryCode);
+  $("#" + codeID).val(phoneCode);
 }
 
 function setUpTel(phoneID, codeID){
-  var inputPhone = document.querySelector("#" + phoneID);
+  $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+    var inputPhone = document.querySelector("#" + phoneID);
+    var countryCode = (resp && resp.country) ? resp.country : "";
+    countryCode = countryCode.toLocaleLowerCase();
     intlTelInput(inputPhone, {
-    separateDialCode: true,
-    initialCountry: "auto"
+      separateDialCode: true,
+      initialCountry: countryCode
+    });
+    $("#" + phoneID).on('keyup click', function () {
+      var number = $(this).val();
+      if(number.length > 0 && number[0] == '0'){
+        $(this).val(number.substring(1));
+      }
+    });
+    inputPhone.addEventListener("keyup", function() {
+      var code = $(this).parent().find(".selected-flag")[0].title.split(": ")[1];
+      if(code !== undefined){
+        $(this).parent().find(".selected-dial-code").html(code);
+        $("#" + codeID).val($("#" + phoneID).parent().find(".selected-flag")[0].title.split("+")[1]);
+      }
+    });
+    inputPhone.addEventListener("close:countrydropdown", function() {
+      var code = $(this).parent().find(".selected-flag")[0].title.split(": ")[1];
+      $(this).parent().find(".selected-dial-code").html(code);
+      $("#" + codeID).val($("#" + phoneID).parent().find(".selected-flag")[0].title.split("+")[1]);
+    });
+    inputPhone.addEventListener("countrychange", function() {
+      var code = $(this).parent().find(".selected-flag")[0].title.split(": ")[1];
+      $(this).parent().find(".selected-dial-code").html(code);
+      $("#" + codeID).val($("#" + phoneID).parent().find(".selected-flag")[0].title.split("+")[1]);
+    });
+    setDefaultCountry(phoneID, codeID, countryCode);
   });
-  $("#" + phoneID).on('keyup click', function () {
-    var number = $(this).val();
-    if(number.length > 0 && number[0] == '0'){
-      $(this).val(number.substring(1));
-    }
-  });
-  inputPhone.addEventListener("countrychange", function() {
-    var code = $(this).parent().find(".selected-flag")[0].title.split(": ")[1];
-    $(this).parent().find(".selected-dial-code").html(code);
-    $("#" + codeID).val($("#" + phoneID).parent().find(".selected-flag")[0].title.split("+")[1]);
-  });
-  setDefaultCountry(phoneID, codeID);
 }
