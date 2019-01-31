1c776e5b1ecf43b2d85a499015e186970e37eb6f
Jirayu Oopipat <jirayu@mohara.co>
2019-01-31 11:59:03 +0000

    Merged in MEDI-596-move-help-menu-to-be-resource-c (pull request #600)
    MEDI-596 move help menu to be resource c
    
    * Deleted unuse form in search bar. ( Complete )
    
    * Fix the input for show patien_email.
    
    * Add help app.
    
    * Hide help link on header.
    
    * Remove useless comment !
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

---
+++ help/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def resource_centre(request):
+    title = 'Resource Centre'
+    return render(request, 'help/menu.html', {
+        'header_title': title,
+    })
