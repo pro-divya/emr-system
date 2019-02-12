1638665b96d01bb6b15578b6871138f9f99ebd60
Jirayu Oopipat <jirayu@mohara.co>
2019-02-12 10:15:06 +0000

    Merged in MEDI-601-update-date-to-uk-format-in-the (pull request #634)
    Edit format time.
    
    * Deleted unuse form in search bar. ( Complete )
    
    * Fix the input for show patien_email.
    
    * Edit format time.
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>

--- instructions/views.py
+++ instructions/views.py
@@ -693,6 +693,7 @@ def consent_contact(request, instruction_id, patient_emis_number):
     patient_email = patient_registration.email() if not patient_instruction.patient_email else patient_instruction.patient_email
     patient_telephone_mobile = patient_registration.mobile_number() if not patient_instruction.patient_telephone_mobile else patient_instruction.patient_telephone_mobile
     patient_alternate_phone = patient_registration.home_telephone() if not patient_instruction.patient_alternate_phone else patient_instruction.patient_alternate_phone
+    date_format = patient_instruction.patient_dob.strftime("%d/%m/%Y")
     # Initial Patient Form
     patient_form = InstructionPatientForm(
         instance=patient_instruction,
@@ -705,7 +706,8 @@ def consent_contact(request, instruction_id, patient_emis_number):
             'patient_email': patient_email,
             'confirm_email' : patient_email,
             'patient_telephone_mobile': patient_telephone_mobile,
-            'patient_alternate_phone': patient_alternate_phone
+            'patient_alternate_phone': patient_alternate_phone,
+            'patient_dob': date_format
         }
     )
     consent_type = 'pdf'
