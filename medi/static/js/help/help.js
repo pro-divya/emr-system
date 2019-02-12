bc3148144beded543d9252fd7a85e3645ca372cb
Mr.Book <jirayu.oopi@gmail.com>
2019-02-12 15:20:17 +0700

    Add video player. and add script for symbol when user click.

--- medi/static/js/help/help.js
+++ medi/static/js/help/help.js
@@ -45,3 +45,39 @@ function changeScreen( code ) {
             break;
     }
 }
+
+let oldClass = 'fas fa-caret-right';
+let newClass = 'fas fa-caret-down';
+$('.choice-1').click(function() {
+    cleanSymbol()
+    $('#symbol-1').removeClass(oldClass);
+    $('#symbol-1').addClass(newClass);
+});
+$('.choice-2').click(function() {
+    cleanSymbol()
+    $('#symbol-2').removeClass(oldClass);
+    $('#symbol-2').addClass(newClass);
+});
+$('.choice-3').click(function() {
+    cleanSymbol()
+    $('#symbol-3').removeClass(oldClass);
+    $('#symbol-3').addClass(newClass);
+});
+$('.choice-4').click(function() {
+    cleanSymbol()
+    $('#symbol-4').removeClass(oldClass);
+    $('#symbol-4').addClass(newClass);
+});
+$('.choice-5').click(function() {
+    cleanSymbol()
+    $('#symbol-5').removeClass(oldClass);
+    $('#symbol-5').addClass(newClass);
+});
+
+function cleanSymbol() {
+    var maxId = 5;
+    for( i = 0; i <= maxId; i++ ) {
+        var idStr = '#symbol-' + i;
+        $(idStr).addClass(oldClass);
+    }
+}
