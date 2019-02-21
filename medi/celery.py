2e7165ec257108f78f5192d65cdc6c0dfcb9af81
Pichate Buakaew <pichate@mohara.co>
2019-02-21 13:11:28 +0000

    Merged in develop (pull request #775)
    Develop
    
    * Merged in MEDI-add-log-to-process-queue (pull request #773)
    
        MEDI add log to process queue
    
        * Update name of queue
    
        * Change text
    
    * Merged in MEDI-add-log-to-process-queue (pull request #774)
    
        MEDI add log to process queue
    
        * Update name of queue
    
        * Change text
    
        * Update setting celery

--- medi/celery.py
+++ medi/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 if os.environ.get('DJANGO_SETTINGS_MODULE'):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('DJANGO_SETTINGS_MODULE'))
 else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medi.settings.local_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medi.settings.prod_settings')
 
 app = Celery('medi')
 app.config_from_object('django.conf:settings', namespace='CELERY')
