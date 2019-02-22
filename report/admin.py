a6cb03a7d58f98a1743ea8ceb5d554a7c20fc4f3
pro-divya <ksso8901@gmail.com>
2019-02-22 05:25:46 +0000

    Merged in MEDI-661-click-expired-to-access-patient (pull request #661)
    send email when medidata admin set authorise expired in admin
    
    Approved-by: Apichate Pipattrisorn <apichate@mohara.co>
    Approved-by: Pichate Buakaew <pichate@mohara.co>

--- report/admin.py
+++ report/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import PatientReportAuth, ThirdPartyAuthorisation
+from django.core.mail import send_mail
 
-# Register your models here.
+
+class ThirdPartyAuthorisationAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+
+        if obj.expired:
+            send_mail(
+                'Medical Report Authorisation',
+                'Your access on SAR report from {patient_name} has been expired. Please contact {patient_name}'.format(
+                    patient_name=obj.patient_report_auth.patient.user.first_name,
+                ),
+                'Medidata',
+                [obj.email],
+                fail_silently=True
+            )
+            super().save_model(request, obj, form, change)
+
+
 admin.site.register(PatientReportAuth)
-admin.site.register(ThirdPartyAuthorisation)
+admin.site.register(ThirdPartyAuthorisation, ThirdPartyAuthorisationAdmin)
