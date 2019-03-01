930cd9ec1c4c50c7110654c32cc57a5246da4a68
pro-divya <ksso8901@gmail.com>
2019-03-01 17:19:10 +0700

    Change GP Fees Structure

--- onboarding/tests/test_functions.py
+++ onboarding/tests/test_functions.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from organisations.models import OrganisationGeneralPractice
-from payment.models import OrganisationFee
+from payment.models import OrganisationFeeRate, GpOrganisationFee
 from ..functions import create_gp_user, create_gp_payments_fee, update_gp_organisation_bank_details
 from ..forms import BankDetailsEmrSetUpStage2Form
 
@@ -16,10 +16,10 @@ class FunctionsOnboardingTest(TestCase):
             'bank_account_name': 'Test Bank account Name',
             'bank_account_number': '11-22-33',
             'bank_account_sort_code': '123456',
-            'received_within_5_days': 60.00,
-            'received_within_6_to_10_days': 51.00,
-            'received_within_11_to_15_days': 45.90,
-            'received_after_15_days': 38.56,
+            'received_within_3_days': 60.00,
+            'received_within_4_to_7_days': 51.00,
+            'received_within_8_to_11_days': 45.90,
+            'received_after_11_days': 38.56,
             'completed_by': 'Completer Man',
             'job_title': 'Tet Job Title'
         })
@@ -33,7 +33,7 @@ class FunctionsOnboardingTest(TestCase):
 
         organisation_fee = create_gp_payments_fee(self.bank_detail_form, self.gp_practice)
 
-        self.assertEqual(True, OrganisationFee.objects.filter(id=organisation_fee.id).exists())
+        self.assertEqual(True, GpOrganisationFee.objects.filter(id=organisation_fee.id).exists())
         self.assertEqual(decimal.Decimal(60.00), organisation_fee.amount_rate_lvl_1)
         self.assertEqual(decimal.Decimal(51.00), organisation_fee.amount_rate_lvl_2)
         self.assertEqual(
