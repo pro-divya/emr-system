4690c30e7c28ad996ba2a389cea4943a454334f9
Mr.Book <jirayu.oopi@gmail.com>
2019-04-05 14:14:26 +0700

    Edit logic.

--- payment/cron/genarate_invoice.py
+++ payment/cron/genarate_invoice.py
@@ -17,7 +17,7 @@ def genarated_weekly_invoice():
     total_cost = 0
     now = timezone.now()
     client_org = OrganisationClient.objects.all()
-    medi_user = OrganisationMedidata.objects.first()
+    invoice_data = list()
     for client in client_org:
         weekly_table = WeeklyInvoice()
         weekly_table.start_date = now - datetime.timedelta(days=7)
@@ -38,7 +38,9 @@ def genarated_weekly_invoice():
                 medi_earn = instruction.medi_earns
                 total_cost = total_cost + gp_earn + medi_earn
 
-        now = timezone.now()
+                invoice_data.append(instruction)
+
+        medi_user = OrganisationMedidata.objects.first()
         date_detail = {
             'date_invoice': now,
             'dute_date': now + timedelta(days=7)
@@ -47,13 +49,13 @@ def genarated_weekly_invoice():
             'client_detail': client,
             'medi_detail': medi_user,
             'date_detail': date_detail,
-            'record': complete_instructions
+            'record': invoice_data
         }
 
         uuid_hex = uuid.uuid4().hex
         weekly_table.weekly_invoice_pdf_file.save('invoice_%s.pdf'%uuid_hex, PaymentInvoice.get_invoice_pdf_file(params))
 
         weekly_table.total_cost = total_cost
-        weekly_table.number_instructions = complete_instructions.count()
+        weekly_table.number_instructions = len(invoice_data)
         weekly_table.paid = False
         weekly_table.save()
