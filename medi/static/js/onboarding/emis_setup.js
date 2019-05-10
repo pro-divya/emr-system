9dab32d32603102e1ce1e1600ce73742d87d3313
blackboat <diemsava@yandex.com>
2019-05-09 23:32:14 -0400

    third onboarding page & fix some issues in 1,2th pages

--- medi/static/js/onboarding/emis_setup.js
+++ medi/static/js/onboarding/emis_setup.js
@@ -36,7 +36,7 @@ function pollingEmis(url) {
                     $(".btn-checkSetup").hide();
                     $(".emisSetupSuccess").removeClass("d-none");
                     setTimeout(function () {
-                        document.location.href = "/onboarding/emr-setup-final/" + data["practice_code"]
+                        document.location.href = "{% url 'accounts:login' %}";
                     }, 2000)
                 } else {
                     $('#checkingEmisButton').prop("disabled", false);
--- onboarding/urls.py
+++ onboarding/urls.py
@@ -10,4 +10,5 @@ urlpatterns = (
     path('emis-polling/<str:practice_code>', views.ajax_emis_polling, name='emis_polling'),
     path('step-1/', views.step1, name='step1'),
     path('step-2/<str:practice_code>', views.step2, name='step2'),
+    path('step-3/<str:practice_code>', views.step3, name='step3'),
 )
