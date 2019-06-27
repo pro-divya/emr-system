39faef56bb1715be004b7b2c92ae44644012f20a
pro-divya <ksso8901@gmail.com>
2019-06-27 05:01:21 -0400

    Made status changable to printed and updated export logic

--- medi/static/import_export/action_formats.js
+++ medi/static/import_export/action_formats.js
@@ -1,22 +1,35 @@
 (function($) {
-  $(document).on('ready', function() {
-    var $actionsSelect, $formatsElement;
-    if ($('body').hasClass('grp-change-list')) {
-        // using grappelli
-        $actionsSelect = $('#grp-changelist-form select[name="action"]');
-        $formatsElement = $('#grp-changelist-form select[name="file_format"]');
-    } else {
-        // using default admin
-        $actionsSelect = $('#changelist-form select[name="action"]');
-        $formatsElement = $('#changelist-form select[name="file_format"]').parent();
-    }
-    $actionsSelect.on('change', function() {
-      if ($(this).val() === 'export_admin_action') {
-        $formatsElement.show();
-      } else {
-        $formatsElement.hide();
-      }
+    $(document).ready(function() {
+        // $(document).on('ready', function() {
+        var $actionsSelect, $formatsElement;
+        if ($('body').hasClass('grp-change-list')) {
+            // using grappelli
+            $actionsSelect = $('#grp-changelist-form select[name="action"]');
+            $formatsElement = $('#grp-changelist-form select[name="file_format"]');
+        } else {
+            // using default admin
+            $actionsSelect = $('#changelist-form select[name="action"]');
+            $formatsElement = $('#changelist-form select[name="file_format"]').parent();
+        }
+        $actionsSelect.on('change', function() {
+            if ($(this).val() === 'export_admin_action') {
+                $formatsElement.show();
+            } else {
+                $formatsElement.hide();
+            }
+        });
+        $actionsSelect.change();
+        $('.export_link').on('click', function(e) {
+            e.preventDefault();
+            $('select[name="action"]').val('export_admin_action');
+            $('select[name="file_format"]').val('0');
+            $('button[name="index"]').val('0');
+
+            setTimeout(function() {
+                window.location.reload();
+            }, 500);
+
+            $('#changelist-form').submit();
+        });
     });
-    $actionsSelect.change();
-  });
 })(django.jQuery);
